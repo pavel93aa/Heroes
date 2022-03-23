7f71d96222fec2951945c30a1d7f9fb78cf9c9ad
pablo <93aa@mail.ru>
2022-03-24 00:27:03 +0300

    Списки заменены на отображения, обновлен метод createMapOfValuesOfCreatures()

--- src/test/java/ru/ag/heroes/Tests.java
+++ src/test/java/ru/ag/heroes/Tests.java
@@ -1,5 +1,6 @@
 package ru.ag.heroes;
 
+import org.json.JSONObject;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
@@ -7,29 +8,16 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.io.*;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.*;
 
 /**
  * Класс содержит простые UI-тесты сайта https://heroes.ag.ru
  */
 public class Tests {
     private WebDriver driver;
-    private final List<String> listOfCreatures = new ArrayList<>();
-    private final List<String> listOfAttack = new ArrayList<>();
-    private final List<String> listOfDefense = new ArrayList<>();
-    private final List<String> listOfDamage = new ArrayList<>();
-    private final List<String> listOfHitPoints = new ArrayList<>();
-    private final List<String> listOfSpeed = new ArrayList<>();
-    private final List<String> listOfInitiative = new ArrayList<>();
-    private final List<String> listOfShots = new ArrayList<>();
-    private final List<String> listOfMana = new ArrayList<>();
-    private final List<String> listOfGrowthRate = new ArrayList<>();
-    private final List<String> listOfGold = new ArrayList<>();
-    private final List<String> listOfResource = new ArrayList<>();
-    private final List<String> listOfExperience = new ArrayList<>();
 
     @BeforeMethod
     public void setUp() {
@@ -76,11 +64,9 @@ public class Tests {
 
     @Test(description = "Получение информации о существах")
     public void getCreaturesInfoTest() {
+        Map<String, Map<String, Map<String, String>>> mapOfFractions = new HashMap<>();
         List<String> listOfFractions = new ArrayList<>();
-        List<List<String>> listOfCreaturesParameters = new ArrayList<>();
         Collections.addAll(listOfFractions, "Academy", "Dungeon", "Haven", "Inferno", "Necropolis", "Sylvan", "Fortress", "Stronghold");
-        Collections.addAll(listOfCreaturesParameters, listOfCreatures, listOfAttack, listOfDefense, listOfDamage, listOfHitPoints, listOfSpeed,
-                listOfInitiative, listOfShots, listOfMana, listOfGrowthRate, listOfGold, listOfResource, listOfExperience);
 
         //Переход по ссылке "Heroes V"
         driver.findElement(By.xpath("//a[text()='Heroes V']")).click();
@@ -88,64 +74,49 @@ public class Tests {
         //Переход по ссылке "Существа"
         driver.findElement(By.xpath("//a[@href='creatures/index.html']")).click();
 
-        for (int i = 0; i < 8; i++) {
-            //Вызываем метод получения информации о существах для каждой из 8 фракций
-            getCreaturesInfo(driver.findElement(By.xpath("//a[text()='" + listOfFractions.get(i) + "']")), listOfFractions.get(i));
+        //Сохранение значений параметров всех существ всех фракций
+        for (String fraction : listOfFractions)
+            mapOfFractions.put(fraction, createMapOfValuesOfCreatures(driver.findElement(By.xpath("//a[text()='" + fraction + "']"))));
 
-            //Сохранение информации о существах в файл
-            try (FileOutputStream fileOutputStream = new FileOutputStream(listOfFractions.get(i) + "Creatures.txt");
-                 OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutputStream)) {
+        JSONObject jsonObject = new JSONObject(mapOfFractions);
 
-                //Проходим циклом for по всем параметрам всех существ
-                //В конце каждого цикла запятая не пишется в поток символов
-                for (List<String> parameters : listOfCreaturesParameters) {
-                    for (int j = 0; j < parameters.size(); j++) {
-                        if (j == parameters.size() - 1) outputStreamWriter.write(parameters.get(j));
-                        else outputStreamWriter.write(parameters.get(j) + ", ");
-                    }
-                    outputStreamWriter.write("\n");
-                    parameters.clear();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        //Сохранение информации о существах в json-файл
+        try (FileOutputStream fileOutputStream = new FileOutputStream("Creatures.json");
+             OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutputStream)) {
+            outputStreamWriter.write(jsonObject.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+
+        //Вывод json в консоль
+        System.out.println(jsonObject);
     }
 
-    public void getCreaturesInfo(WebElement webElement, String fraction) {
+    public Map<String, Map<String, String>> createMapOfValuesOfCreatures(WebElement webElement) {
+        Map<String, Map<String, String>> mapOfValuesOfCreatures = new HashMap<>();
+
         //Переход по ссылке фракции
         webElement.click();
 
-        //Сохраняем параметры всех существ фракции
+        //Сохранение значений параметров всех существ фракции
         for (int number = 1; number <= 21; number++) {
-            listOfCreatures.add(driver.findElement(By.xpath("(//div[@class='header1'])[" + number + "]")).getText());
-            listOfAttack.add(driver.findElement(By.xpath("(//td[@class='cbg2'])[" + number + "]//tr[1]//td[@class='cells'][1]")).getText());
-            listOfDefense.add(driver.findElement(By.xpath("(//td[@class='cbg2'])[" + number + "]//tr[2]//td[@class='cells'][1]")).getText());
-            listOfDamage.add(driver.findElement(By.xpath("(//td[@class='cbg2'])[" + number + "]//tr[3]//td[@class='cells'][1]")).getText());
-            listOfHitPoints.add(driver.findElement(By.xpath("(//td[@class='cbg2'])[" + number + "]//tr[4]//td[@class='cells'][1]")).getText());
-            listOfSpeed.add(driver.findElement(By.xpath("(//td[@class='cbg2'])[" + number + "]//tr[1]//td[@class='cells'][2]")).getText());
-            listOfInitiative.add(driver.findElement(By.xpath("(//td[@class='cbg2'])[" + number + "]//tr[2]//td[@class='cells'][2]")).getText());
-            listOfShots.add(driver.findElement(By.xpath("(//td[@class='cbg2'])[" + number + "]//tr[3]//td[@class='cells'][2]")).getText());
-            listOfMana.add(driver.findElement(By.xpath("(//td[@class='cbg2'])[" + number + "]//tr[4]//td[@class='cells'][2]")).getText());
-            listOfGrowthRate.add(driver.findElement(By.xpath("(//td[@class='cbg2'])[" + number + "]//tr[1]//td[@class='cells'][3]")).getText());
-            listOfGold.add(driver.findElement(By.xpath("(//td[@class='cbg2'])[" + number + "]//tr[2]//td[@class='cells'][3]")).getText());
-            listOfResource.add(driver.findElement(By.xpath("(//td[@class='cbg2'])[" + number + "]//tr[3]//td[@class='cells'][3]")).getText());
-            listOfExperience.add(driver.findElement(By.xpath("(//td[@class='cbg2'])[" + number + "]//tr[4]//td[@class='cells'][3]")).getText());
+            Map<String, String> mapOfValuesOfCreaturesParameters = new HashMap<>();
+
+            mapOfValuesOfCreaturesParameters.put("attack", driver.findElement(By.xpath("(//td[@class='cbg2'])[" + number + "]//tr[1]//td[@class='cells'][1]")).getText());
+            mapOfValuesOfCreaturesParameters.put("defense", driver.findElement(By.xpath("(//td[@class='cbg2'])[" + number + "]//tr[2]//td[@class='cells'][1]")).getText());
+            mapOfValuesOfCreaturesParameters.put("damage", driver.findElement(By.xpath("(//td[@class='cbg2'])[" + number + "]//tr[3]//td[@class='cells'][1]")).getText());
+            mapOfValuesOfCreaturesParameters.put("hitPoints", driver.findElement(By.xpath("(//td[@class='cbg2'])[" + number + "]//tr[4]//td[@class='cells'][1]")).getText());
+            mapOfValuesOfCreaturesParameters.put("speed", driver.findElement(By.xpath("(//td[@class='cbg2'])[" + number + "]//tr[1]//td[@class='cells'][2]")).getText());
+            mapOfValuesOfCreaturesParameters.put("initiative", driver.findElement(By.xpath("(//td[@class='cbg2'])[" + number + "]//tr[2]//td[@class='cells'][2]")).getText());
+            mapOfValuesOfCreaturesParameters.put("shots", driver.findElement(By.xpath("(//td[@class='cbg2'])[" + number + "]//tr[3]//td[@class='cells'][2]")).getText());
+            mapOfValuesOfCreaturesParameters.put("mana", driver.findElement(By.xpath("(//td[@class='cbg2'])[" + number + "]//tr[4]//td[@class='cells'][2]")).getText());
+            mapOfValuesOfCreaturesParameters.put("growthRate", driver.findElement(By.xpath("(//td[@class='cbg2'])[" + number + "]//tr[1]//td[@class='cells'][3]")).getText());
+            mapOfValuesOfCreaturesParameters.put("gold", driver.findElement(By.xpath("(//td[@class='cbg2'])[" + number + "]//tr[2]//td[@class='cells'][3]")).getText());
+            mapOfValuesOfCreaturesParameters.put("resource", driver.findElement(By.xpath("(//td[@class='cbg2'])[" + number + "]//tr[3]//td[@class='cells'][3]")).getText());
+            mapOfValuesOfCreaturesParameters.put("experience", driver.findElement(By.xpath("(//td[@class='cbg2'])[" + number + "]//tr[4]//td[@class='cells'][3]")).getText());
+
+            mapOfValuesOfCreatures.put(driver.findElement(By.xpath("(//div[@class='header1'])[" + number + "]")).getText(), mapOfValuesOfCreaturesParameters);
         }
-        //Вывод параметров в консоль
-        System.out.println(fraction);
-        System.out.println("Существо " + listOfCreatures);
-        System.out.println("Атака " + listOfAttack);
-        System.out.println("Защита " + listOfDefense);
-        System.out.println("Урон " + listOfDamage);
-        System.out.println("Здоровье " + listOfHitPoints);
-        System.out.println("Скорость " + listOfSpeed);
-        System.out.println("Инициатива " + listOfInitiative);
-        System.out.println("Выстрелы " + listOfShots);
-        System.out.println("Мана " + listOfMana);
-        System.out.println("Прирост " + listOfGrowthRate);
-        System.out.println("Золото " + listOfGold);
-        System.out.println("Ресурс " + listOfResource);
-        System.out.println("Опыт " + listOfExperience);
+        return mapOfValuesOfCreatures;
     }
 }
